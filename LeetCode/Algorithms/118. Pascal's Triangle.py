6e4b976ca0a0af9153bc5ee575c960008865fda0
David Ambrozio <davidambroziodev@gmail.com>
2023-12-04 20:46:40 -0300

    118. Pascal's Triangle

---
+++ LeetCode/Algorithms/118. Pascal's Triangle.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+
+        result = []
+        first = [1]
+        result.append(first)
+
+        for i in range(1, numRows):
+            prev = result[i - 1]
+            current = [1]
+
+            for j in range(1, i):
+                current.append(prev[j - 1] + prev[j])
+
+            current.append(1)
+            result.append(current)
+
+        return result
