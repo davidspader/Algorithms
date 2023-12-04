16f0770ae6ea10b2ec61d633ba9eea633920005e
David Ambrozio <davidambroziodev@gmail.com>
2023-12-03 21:50:37 -0300

    136. Single Number

--- LeetCode/Algorithms/136. Single Number.py
+++ LeetCode/Algorithms/136. Single Number.py
@@ -1,7 +1,7 @@
 from typing import List
 
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
+    def singleNumber(nums: List[int]) -> int:
         for i in range(len(nums)):
             valueCount = 0
             for j in range(len(nums)):
@@ -10,3 +10,16 @@ class Solution:
             
             if valueCount == 1:
                 return nums[i]
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor = 0
+        for num in nums:
+            xor ^= num
+        return xor
+    
+'''
+Algorithm 2 (XOR approach) is more efficient and clearer compared to Algorithm 1. 
+It has a linear time complexity and is generally preferred for solving this type of problem.
+'''
